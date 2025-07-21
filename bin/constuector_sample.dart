808fee483be5cb233082546cd1ea5b91ae38972e
shihabLuminar <neethu@luminartechnolab.com>
2025-07-21 12:11:38 +0530

    first commit

---
+++ bin/constuector_sample.dart
@@ -0,0 +1,42 @@
+class Student {
+  Student({
+    required this.name,
+    required this.age,
+    required this.grade,
+    this.isPassed = false,
+  }) {
+    print("$name object created");
+  }
+
+  String name;
+  int age;
+  String grade;
+  bool isPassed;
+
+  displayInfo() {
+    print("Name : $name");
+    print("Age : $age");
+    print("Grade : $grade");
+    print("Is Passed : $isPassed");
+  }
+
+  checkPassStatus() {
+    if (isPassed) {
+      print("Student has passed");
+    } else {
+      print("Student has not passed");
+    }
+  }
+}
+
+void main() {
+  Student shihab = Student(name: "shihab", age: 10, grade: "A", isPassed: true);
+  Student anand = Student(name: "anand", age: 10, grade: "c");
+
+  shihab.displayInfo();
+  shihab.checkPassStatus();
+
+  print("-----------------------------");
+  anand.displayInfo();
+  anand.checkPassStatus();
+}
---
+++ bin/oops_sample_june_25.dart
@@ -0,0 +1,32 @@
+// class is a blueprint of an object
+// class is a collection of data members and member funcitons
+// class is a collection of properties and methods
+// class is a user defined data type
+
+class Vehicle {
+  //class properties
+  int speed = 100;
+  String color = "red";
+
+  // methods
+  void startCar() {
+    print("car is starting");
+  }
+}
+
+void main(List<String> args) {
+  Vehicle v1 = Vehicle(); // object
+  Vehicle v2 = Vehicle(); // object
+  // object is an instance of class
+  // constructor is a special method used to initialize an object when it is created , and it runs automatically
+
+  v1.color = "blue";
+  v1.speed = 10;
+  print(v1.speed);
+  print(v1.color);
+  v1.startCar();
+
+  print(v2.speed);
+  print(v2.color);
+  v2.startCar();
+}
